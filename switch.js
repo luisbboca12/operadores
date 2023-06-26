0437c6f137fdda69773b8c0dae10be3ad350e829
luisbboca12 <107447827+luisbboca12@users.noreply.github.com>
2023-06-26 18:06:49 -0300

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,17 @@
+var numero= 1
+    switch (numero) {
+        case 1:
+            return "usted us computadora"
+            break;
+    
+        case 2:
+            return console.log("usted usa celular")
+
+            break;
+        case 3:
+            return console.log("cable")
+            break;
+
+        default:
+        return console.log("nada")
+    }
